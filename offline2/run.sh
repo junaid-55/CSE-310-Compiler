58add876c4c13bcfebae7c2a0fbb10b43d424469
junaid_55 <saikat12340@gmail.com>
2025-05-16 18:26:21 +0600

    Changes made in linux

--- offline2/run.sh
+++ offline2/run.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/bash
 
 file="draft.l"
-powershell.exe -NoProfile -Command "flex '$file'"
-
+# powershell.exe -NoProfile -Command "flex '$file'"
+echo "Compiling $file"
+flex $file
 if [ -f "lex.yy.c" ]; then
   g++ lex.yy.c -o test
   rm lex.yy.c
   ./test input.txt
 fi
-rm test.exe
+rm test
