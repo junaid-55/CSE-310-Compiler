9381c2b72352694788b6aaaa4b3df386d409f8d6
junaid_55 <saikat12340@gmail.com>
2025-05-18 13:23:30 +0600

    final changes from linux branch

--- offline2/run.sh
+++ offline2/run.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/bash
 
-file="draft.l"
+
+file="main.l"
+# for windows
 # powershell.exe -NoProfile -Command "flex '$file'"
-echo "Compiling $file"
+
+# for linux
+flex $file
+
 flex $file
 if [ -f "lex.yy.c" ]; then
-  g++ lex.yy.c -o test
+  g++ -fsanitize=address lex.yy.c -o test
   rm lex.yy.c
   ./test input.txt
 fi
